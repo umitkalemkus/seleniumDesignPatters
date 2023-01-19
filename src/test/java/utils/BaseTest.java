81913e467e26688f9afea6294f603e7c53d5ea30
umitkalemkus <umitkalemkus@yahoo.com>
2023-01-19 18:28:40 +0000

    Initial commit

---
+++ src/test/java/utils/BaseTest.java
@@ -0,0 +1,35 @@
+package utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+
+public class BaseTest {
+
+
+    protected WebDriver driver;
+    protected Actions actions;
+
+
+    @BeforeMethod
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        actions = new Actions(driver);
+    }
+
+    @AfterMethod
+    public void teardown() {
+        //driver.quit();
+    }
+
+
+}
+
